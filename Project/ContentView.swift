1784ff7a9115b7c060088b7a7c60503d8c7145bd
“peterpark” <peterpark001221@gmail.com>
2023-06-26 10:06:13 +0900

    Initial Commit

---
+++ Project/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Project
+//
+//  Created by PeterPark on 2023/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("HI swift")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project/ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectApp.swift
+//  Project
+//
+//  Created by PeterPark on 2023/06/26.
+//
+
+import SwiftUI
+
+@main
+struct ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
